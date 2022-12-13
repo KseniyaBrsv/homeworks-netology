2b07d8055b6c074c8811434ae0c9ba0cd48846b1
KseniyaBrsv <Kseniya_borisova00@mail.ru>
2022-12-13 15:11:06 +0400

    Finish

--- shoe-store/work-seven-time/src/components/VideoList.js
+++ shoe-store/work-seven-time/src/components/VideoList.js
@@ -2,10 +2,11 @@ import moment from "moment";
 import {Video} from "./Video";
 
 const dateTimePretty = (Component) => {
-  return (props) => {
+   const Wrapper = (props) => {
     const fromNow = moment(props.date).locale('ru').fromNow();
     return <Component {...props} {...{ date: fromNow }} />;
   };
+   return Wrapper;
 };
 
 const UpgradedVideo = dateTimePretty(Video);
