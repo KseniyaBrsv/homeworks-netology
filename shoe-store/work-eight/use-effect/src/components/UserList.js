92bc038b9d2d1e0207240968332124e1cf8c9fc5
KseniyaBrsv <Kseniya_borisova00@mail.ru>
2022-12-22 17:49:59 +0300

    Finish

---
+++ shoe-store/work-eight/use-effect/src/components/UserList.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+import Loader from './Loader';
+import { ListGroup } from 'react-bootstrap';
+
+const UserList = ({ getUserId }) => {
+    const [users, setUsers] = useState(null);
+    const [error, setError] = useState(null);
+    const [isLoading, setLoading] = useState(true);
+
+    useEffect(() => {
+      fetch('https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/hooks-context/use-effect/data/users.json')
+        .then((response) => response.json())
+        .then((result) => {
+          setUsers([...result]);
+        })
+        .catch((err) => {
+          setError(err.message);
+        })
+        .finally(() => setLoading(false));
+    }, []);
+
+    return (
+      <>
+        {isLoading ? (
+          <Loader />
+        ) : error ? (
+          <p>{error}</p>
+        ) : (
+          users.length > 0 && (
+            <ListGroup>
+              {users.map((user) => (
+                <ListGroup.Item
+                  action
+                  onClick={() => getUserId(user.id)}
+                  key={user.id}
+                >
+                  {user.name}
+                </ListGroup.Item>
+              ))}
+            </ListGroup>
+          )
+        )}
+      </>
+    );
+};
+
+export default UserList;
